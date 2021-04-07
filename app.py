300e13420c3384a32023a3c3d282cfceb4948e5b
gunnarsikorski <gunnarsikorski@gmail.com>
2021-04-07 14:53:04 -0500

    Adding conditionals in case of improper typecase

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open('data.json'))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return 'This word doesn\'t exist. Please try again'
 
 user_word = input('Enter a word: ')
 
